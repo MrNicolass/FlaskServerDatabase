079854be57814d5bc2034ddf2478409d76ad3897
Nicolas Gustavo Conte <80847876+MrNicolass@users.noreply.github.com>
2023-12-01 21:35:38 -0300

    Feat: User Registration
    Were added the function to create new users and the basic inputs, also, the base HTML for other pages.

--- server/database/dbconnection.py
+++ server/database/dbconnection.py
@@ -1,18 +1,34 @@
 import mysql.connector
+from main import flash
 
-#Define a conexão do banco de dados
-cnx = mysql.connector.connect(
-    host="localhost",
-    port="3306",
-    user="root",
-    password="MtMsH@+6XDZi"
-)
+def user_insert(email,nome,sobrenome,ativo):
+    #Define a conexão do banco de dados
+    cnx = mysql.connector.connect(
+        host="localhost",
+        port="3306",
+        user="root",
+        password="MtMsH@+6XDZi",
+        database='n3'
+    )
+    
+    #Define uma variável para facilicar a utilização do cursos de banco de dados
+    dbcursor = cnx.cursor()
 
-#Define uma variável para facilicar a utilização do cursos de banco de dados
-dbcursor = cnx.cursor()
+    #Tenta realizar operação de inserção de um novo usuário
+    try:
+        insert = f"INSERT INTO usuarios (email, nome, sobrenome, ativo) VALUES (%s, %s, %s, %s)"
+        values = [email, nome, sobrenome, ativo]
+        dbcursor.execute(insert, values)
 
-def teste():
-    dbcursor.execute("select * from arte.artistas")
-    myresult = dbcursor.fetchall()
-    for x in myresult:
-        print(x)
+        #Realiza operação de gravar os dados na tabela
+        cnx.commit()
+        flash("Usuário cadastrado com sucesso!", "Sucesso")
+
+    #Caso não consiga inserir, pega e exibe mensagem de erro do banco de dados
+    except mysql.connector.Error as error:
+        flash(f"Falha ao inserir dados, erro: {error}", "Erro")
+
+    finally:
+        if cnx.is_connected():
+            dbcursor.close()
+            cnx.close()
--- server/main.py
+++ server/main.py
@@ -6,10 +6,32 @@ import os
 template_dir = os.path.abspath('client/src/pages')
 #Variável/instância para acessar o Flask de forma mais fácil
 app = Flask(__name__, template_folder=template_dir)
+app.config["SECRET_KEY"] = "random string"
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/users", methods=['GET', 'POST'])
+def users():
+    if request.method == 'POST':
+        if not request.form['email'] or not request.form['nome'] or not request.form['sobrenome'] or not request.form['ativo']:
+            flash("Preencha todos os campos", "Erro")
+        else:
+            email = request.form['email']
+            nome = request.form['nome']
+            sobrenome = request.form['sobrenome']
+            ativo = int(request.form['ativo'])
+            dbc.user_insert(email, nome, sobrenome, ativo)
+    return render_template("users.html")
+
+@app.route("/products")
+def products():
+    return render_template("products.html")
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
 
 if __name__ == "__main__":
     #Cria o contexto da aplicação
